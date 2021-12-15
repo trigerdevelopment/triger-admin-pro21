2c530ac3268dde143401970189338bd33d10f086
trigerdevelopment <triger.development.team@gmail.com>
2021-12-15 14:30:51 -0600

    configure to deploy on heroku

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/triger-admin-pro20'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/triger-admin-pro20/index.html'));});
+res.sendFile(path.join(__dirname+'/dist/triger-admin-pro20/index.html'));});
 app.listen(process.env.PORT || 8080);
